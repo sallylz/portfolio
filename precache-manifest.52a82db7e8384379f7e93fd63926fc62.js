0dc00acbb3e6e0f6d1678dc0324d287e0e903613
Sally <sllzhao@ucdavis.edu>
2020-09-29 00:08:54 -0700

    Updates

---
+++ precache-manifest.52a82db7e8384379f7e93fd63926fc62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f9bd4cea6b61f3e9ad9f51be866cd80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d95bce8e2deb696e93d5",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b97743b1076a4f90e2bf",
+    "url": "/static/js/2.8f18329a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8f18329a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d95bce8e2deb696e93d5",
+    "url": "/static/js/main.e18103aa.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
